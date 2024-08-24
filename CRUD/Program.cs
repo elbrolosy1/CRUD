f2a904a183d9b9c154c401760b3d403753e1d0d5
Ziad elbrolosy <zeyadelbrolosy5566@gmail.com>
2024-08-24 12:27:39 +0300

    CRUD project

---
+++ CRUD/Program.cs
@@ -0,0 +1,44 @@
+﻿using CRUD;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<user> list = new List<user>() 
+        {
+            //sampleData
+            new user{Name="SD",Email="Assiut",Phone="011"},
+            new user{Name="Unix",Email="Alex",Phone="012"},
+            new user{Name="Java",Email="Cairo",Phone="013"},
+            new user{Name="E-Learning",Email="Alex",Phone="014"}
+        };
+         user user = new user();  
+        while (true)
+        {
+            Console.WriteLine("Welecome in my first task ");
+            Console.WriteLine("what do you want ?\n 1) Add \n 2)Update  \n 3) Delete\n 4) display \n 5)Exit");
+            int chos=int.Parse(Console.ReadLine());
+            if (chos == 1) 
+            {
+                user.AddUser(list);
+            }
+            else if (chos == 2)
+            {
+                user.UpdateUser(list);
+
+            }
+            else if (chos == 3)
+            {
+                user.RemoveUser(list);
+            }
+            else if (chos == 4)
+            {
+                user.DisplayUser(list);
+            }
+            else if (chos == 5)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ CRUD/user.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD
+{
+    internal class user
+    {
+        public string Name { get; set; }
+        public string Email{ get; set; }
+        public string Phone { get; set; }
+
+        public void AddUser(List<user> users) 
+        {
+            Console.WriteLine("=========== Add new user ==============");
+
+            var user=new user();
+
+            Console.WriteLine("please add your Name : ");
+            user.Name = Console.ReadLine();
+
+            Console.WriteLine("please add your Email : ");
+            user.Email = Console.ReadLine();
+
+            Console.WriteLine("please add your Phone : ");
+            user.Phone = Console.ReadLine();
+
+            users.Add(user);
+
+            Console.WriteLine("=========== user information ==============");
+
+            Console.WriteLine($"Name : {user.Name}");
+            Console.WriteLine($"Email : {user.Email}");
+            Console.WriteLine($"Phone : {user.Phone}");
+
+        }
+
+        public void UpdateUser(List<user> users)
+        {
+            Console.WriteLine("Enter phone number to find user : ");
+            string findphone = Console.ReadLine();
+            var founded = users.FirstOrDefault(x => x.Phone == findphone);
+            if (founded != null)
+            {
+                Console.WriteLine("information of user ");
+                Console.WriteLine($"Name :{founded.Name} ,\n" +
+                                  $" Email : {founded.Email}\n" +
+                                  $" and phone{founded.Phone}");
+                up:
+                Console.Write("what do you need to update ?\n 1) Name \n 2) Email \n 3) Phone");
+                Console.WriteLine("select number :");
+                int chos=int.Parse(Console.ReadLine());
+                if (chos==1)
+                {
+                    Console.WriteLine("what is new name ? ");
+                    string newname = Console.ReadLine();
+                    founded.Name = newname;
+                }
+                else if (chos==2) 
+                {
+                    Console.WriteLine("what is new Email ? ");
+                    string newEmail = Console.ReadLine();
+                    founded.Email = newEmail;
+                }
+                else if(chos==3) 
+                {
+                    Console.WriteLine("what is new phone ? ");
+                    string newphone = Console.ReadLine();
+                    founded.Phone = newphone;
+                }
+                Console.WriteLine("do you want another update ? (Y/N)");
+                char chos2=char.Parse(Console.ReadLine().ToLower());
+                if (chos2 == 'y')
+                {
+                    goto up;
+                }
+                else
+                {
+                    Console.WriteLine("your updates are done ...");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("No user with this phone number ");
+            }
+        }
+
+        public void RemoveUser(List<user> users)
+        {
+            Console.WriteLine("Enter phone number to find user : ");
+            string findphone = Console.ReadLine();
+            var founded = users.FirstOrDefault(x => x.Phone == findphone);
+            if (founded != null) 
+            {
+                users.Remove(founded);
+                Console.WriteLine("user is deleted");
+            }
+            else
+            {
+                Console.WriteLine("no user found");
+                    
+            }
+
+
+        }
+
+        public void DisplayUser(List<user> users)
+        {
+            int count = 1;
+            foreach (user user in users)
+            {
+                if (users.Count == 0)
+                {
+                    Console.WriteLine("=================================");
+                    Console.WriteLine("no users found");
+                }
+                else
+                {
+                    Console.WriteLine("=========== user information ==============");
+                    Console.WriteLine($"user number :{count}");
+                    Console.WriteLine($"Name : {user.Name}");
+                    Console.WriteLine($"Email : {user.Email}");
+                    Console.WriteLine($"Phone : {user.Phone}");
+                    count++;
+                }
+            }
+        }
+    }
+}
